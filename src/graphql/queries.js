274f73a929a824f93c06b640de7fc8caf8a4d011
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-09 01:51:29 -0600

    update

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -89,9 +89,6 @@ export const listBooks = /* GraphQL */ `
         author
         featured
         price
-        orders {
-          nextToken
-        }
         createdAt
         updatedAt
       }
